391177b427c8f3b31aacaa155f9eefe469069875
rizalmab <rizalmahadiab@gmail.com>
2021-12-27 16:51:27 +0800

    transferred parts of gamelogic.js to app.js

--- app.js
+++ app.js
@@ -1,56 +1,76 @@
 /*----- constants -----*/
-let prevVal, currentVal;
+class Card {
+  constructor(value, suit) {
+    this.value = value;
+    this.suit = suit;
+  }
+}
+
+const VALUES = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A",
+];
+const SUITS = ["♠", "♥", "♦", "♣"];
+
 /*----- app's state (variables) -----*/
-let playerCardCount = 26;
-let computerCardCount = 26;
-let discardPileCount = 0;
+let playerCount, computerCount, discardCount;
+let playerDeck, computerDeck;
+let previousCard, currentCard;
+let turn = 0;
+
 /*----- cached element references -----*/
+const startButton = $("#start-button");
+const rulesButton = $("#rules-button");
+const rulesBackButton = $("#rules-back-button");
+const goButton = $("#go-button");
+const snapButton = $("#snap-button");
+
 /*----- functions -----*/
+const showStartScreen = () => {
+  $(".screen").hide();
+  $("#start-screen").show();
+};
 
-const main = () => {
-  const showStartScreen = () => {
-    $(".screen").hide();
-    $("#start-screen").show();
-  };
+const showInputScreen = () => {
+  $(".screen").hide();
+  $("#input-screen").show();
+};
 
-  const showInputScreen = () => {
-    $(".screen").hide();
-    $("#input-screen").show();
-  };
+const showRulesScreen = () => {
+  $(".screen").hide();
+  $("#rules-screen").show();
+};
 
-  const showRulesScreen = () => {
-    $(".screen").hide();
-    $("#rules-screen").show();
-  };
+const showGameScreen = () => {
+  $(".screen").hide();
+  $("#game-screen").show();
+};
 
-  const showGameScreen = () => {
-    $(".screen").hide();
-    $("#game-screen").show();
-  };
+const checkConditions = () => {
+  // code block - make simple condition
+  // if prev card === current card (ie. value is the same)
+  // const prevVal =
 
-  const checkConditions = () => {
-    // code block - make simple condition
-    // if prev card === current card (ie. value is the same)
-    // const prevVal =
-  };
+  const main = () => {};
 
   /*----- event listeners -----*/
 
-  //When click 'start game' button
-  $("#start-button").on("click", showInputScreen);
-
-  // On click of 'rules' button
-  $("#rules-button").on("click", showRulesScreen);
-
-  // On click of 'back' button on Rules page
-  $("#rules-back-button").on("click", showStartScreen);
-
-  // On click of 'go' button after typing name
-  $("#go-button").on("click", showGameScreen);
-
-  // On click of 'snap' button
-  // on click, do function: check conditions for correct snap, change variables (cards)
-  $("#snap-button").on("click", checkConditions);
+  startButton.on("click", showInputScreen);
+  rulesButton.on("click", showRulesScreen);
+  rulesBackButton.on("click", showStartScreen);
+  goButton.on("click", showGameScreen);
+  snapButton.on("click", checkConditions);
 
   // $("h1").innerText("hello");
   // RENDER FUNCTION
